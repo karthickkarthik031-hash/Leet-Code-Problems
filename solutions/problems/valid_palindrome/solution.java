3ec7c44048f92df4025103999d28761bb937ed17
karthickkarthik031-hash <karthickkarthik031@gmail.com>
2025-09-30 04:03:32 +0000

    Sync LeetCode submission - Valid Palindrome (java)

---
+++ solutions/problems/valid_palindrome/solution.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+      String c = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String r = new StringBuilder(c).reverse().toString();
+        return c.equals(r);
+    }
+}
