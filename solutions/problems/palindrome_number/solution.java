4856f6ab9ae8939c86613b1577f719e49354432e
karthickkarthik031-hash <karthickkarthik031@gmail.com>
2026-06-12 06:55:32 +0000

    Sync LeetCode submission - Palindrome Number (java)

--- solutions/problems/palindrome_number/solution.java
+++ solutions/problems/palindrome_number/solution.java
@@ -1,18 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int sum =0;
-        int a = x;
-        if(x<0){
-            return false;
-        }
-        while(x!=0){
-            sum=sum*10+(x%10);
+        int b=x;
+        int sum=0;
+        while(x>0){
+            sum=sum*10+x%10;
             x=x/10;
         }
-        if(sum==a){
+        if(sum==b){
             return true;
         }
-        else
         return false;
     }
 }
