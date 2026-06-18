b528c47e1b239eb147d26abb07cb41713c72936c
karthickkarthik031-hash <karthickkarthik031@gmail.com>
2026-06-18 14:09:04 +0000

    Sync LeetCode submission - Length of Last Word (java)

--- solutions/problems/length_of_last_word/solution.java
+++ solutions/problems/length_of_last_word/solution.java
@@ -1,13 +1,13 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int length = 0;
+        int count = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
             if (s.charAt(i) != ' ') {
-                length++;
-            } else {
-                if (length > 0) return length;
+                count++;
+            } else if (count > 0) {
+                 break;
             }
         }
-        return length;
+        return count;
     }
 }
