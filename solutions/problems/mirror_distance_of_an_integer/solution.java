5b68e5541b57b63f51432cd0f5060377f7e47252
karthickkarthik031-hash <karthickkarthik031@gmail.com>
2026-06-27 10:21:28 +0000

    Sync LeetCode submission - Mirror Distance of an Integer (java)

---
+++ solutions/problems/mirror_distance_of_an_integer/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mirrorDistance(int n) {
+        int sum=0;
+        int b=0;
+        int c=n;
+        while(n>0){
+            int a=n%10;
+            sum=sum*10+a;
+            n=n/10;
+        }
+        b=sum-c;
+        if(b<0){
+            b=b*(-1);
+        }
+        return b;
+    }
+}
