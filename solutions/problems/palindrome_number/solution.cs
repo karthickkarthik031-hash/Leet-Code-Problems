30098d8132451efbffe86500f7728d9d7c810eef
karthickkarthik031-hash <karthickkarthik031@gmail.com>
2026-07-31 02:18:32 +0000

    Sync LeetCode submission - Palindrome Number (csharp)

---
+++ solutions/problems/palindrome_number/solution.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+        int r = 0;
+        int c = x;
+        while (c > 0)
+        {
+            r = r * 10 + c % 10;
+            c /= 10;
+        }
+        return r == x; 
+    }
+}
